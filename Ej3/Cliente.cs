f68f8d00e3020fda2eb02d3598a8fbe3a8a9b11a
Facundo <farebord@gmail.com>
2016-09-16 20:17:55 -0300

    Agregue algunas propiedades de Cliente.

--- Ej3/Cliente.cs
+++ Ej3/Cliente.cs
@@ -11,6 +11,36 @@ namespace Ej3
     {
         private string iNombre;
         private string iApellido;
+		private Empleo iEmpleo;
+		private int iTipoCliente;
         private DateTime iFechaNacimiento;
+
+		public Cliente(string pNombre, string pApellido, DateTime pFechaNacimiento, Empleo pEmpleo)
+		{
+			this.iNombre = pNombre;
+			this.iApellido = pApellido;
+			this.iFechaNacimiento = pFechaNacimiento;
+			this.iEmpleo = pEmpleo;
+		}
+
+		public string Nombre
+		{
+			get { return this.iNombre; }
+		}
+
+		public string Apellido
+		{
+			get { return this.iApellido; }
+		}
+
+		public DateTime FechaNacimiento
+		{
+			get { return this.iFechaNacimiento; }
+		}
+
+		public Empleo Empleo
+		{
+			get { return this.iEmpleo; }
+		}
     }
 }
