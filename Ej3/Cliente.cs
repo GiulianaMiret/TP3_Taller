e36fe604383b7661020ed453e183166524f578aa
GiulianaMiret <dariela_77@hotmail.com>
2016-09-16 20:02:13 -0300

    Ej 3

---
+++ Ej3/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3
+{
+    enum TipoCliente { NoCliente, Cliente, ClienteGold, ClientePremium}
+    class Cliente
+    {
+        private string iNombre;
+        private string iApellido;
+        private DateTime iFechaNacimiento;
+    }
+}
--- Ej3/Program.cs
+++ Ej3/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ej1
+namespace Ej3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Veterinaria.AceptarAnimales(new Animal[]{ new Perro(), new Gato() });
-            Console.ReadKey();
         }
     }
 }
