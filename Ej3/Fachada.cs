a1616fe9357da09a7459fa4f023cc22926db844f
Facundo <farebord@gmail.com>
2016-09-30 20:25:31 -0300

    Listooo

---
+++ Ej3/Fachada.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Ej3
+{
+	public class Fachada
+	{
+		List<Cliente> clientes = new List<Cliente>();
+		GestorPrestamos gPrestamos = new GestorPrestamos();
+
+		public Fachada()
+		{
+			// Cliente 1 tipo No Cliente
+			clientes.Add(new Cliente("Juan", "Perez", new DateTime(1995, 5, 12),
+									 new Empleo(5500.50, new DateTime(2014, 5, 3))));
+			clientes[clientes.Count - 1].TipoCliente = TipoCliente.NoCliente;
+		}
+
+		public bool existeCliente(int cliente)
+		{
+			return cliente >= 0 && cliente < clientes.Count;
+		}
+
+		public double getMaximoMonto(int idCliente)
+		{
+			if(existeCliente(idCliente)){
+				switch (clientes[idCliente].TipoCliente)
+				{
+					case TipoCliente.NoCliente:
+						return 20000;
+					case TipoCliente.Cliente:
+						return 100000;
+					case TipoCliente.ClienteGold:
+						return 150000;
+					case TipoCliente.ClientePremium:
+						return 200000;
+				}
+			}
+			return -1;
+		}
+
+		public int getMaximasCuotas(int idCliente)
+		{
+			if (existeCliente(idCliente){
+				switch (clientes[idCliente].TipoCliente)
+				{
+					case TipoCliente.NoCliente:
+						return 12;
+					case TipoCliente.Cliente:
+						return 32;
+					case TipoCliente.ClienteGold:
+						return 60;
+					case TipoCliente.ClientePremium:
+						return 60;
+				}
+			}
+			return -1;
+		}
+
+		public bool SolicitudPrestamo(int idCliente, double pMonto, int pCuotas)
+		{
+			SolicitudPrestamo solicitud = new SolicitudPrestamo(clientes[idCliente], pMonto, pCuotas);
+			return gPrestamos.EsValida(solicitud);
+		}
+	}
+}
--- Ej3/Program.cs
+++ Ej3/Program.cs
@@ -10,7 +10,54 @@ namespace Ej3
     {
         static void Main(string[] args)
         {
+			Fachada fachada = new Fachada();
+			Console.Clear();
+			Console.WriteLine("Seleccione opcion: ");
+			Console.WriteLine("\t1. Solicitar prestamo");
 
+			char opcion = Console.ReadKey(true).KeyChar;
+
+			Console.Clear();
+
+			switch (opcion)
+			{
+				case '1':
+					{
+						Console.Write("Ingrese ID de Cliente: ");
+						int idCliente = Convert.ToInt32(Console.ReadLine());
+						while (fachada.existeCliente(idCliente))
+						{
+							Console.WriteLine("El cliente no es valido. Intente de nuevo.");
+							Console.Write("Ingrese ID de Cliente: ");
+							idCliente = Convert.ToInt32(Console.ReadLine());
+						}
+						Console.Write("Ingrese monto a solicitar: ");
+						double monto = Convert.ToDouble(Console.ReadLine());
+
+						Console.Write("Ingrese cuotas a solicitar: ");
+						int cuotas = Convert.ToInt32(Console.ReadLine());
+
+						if (fachada.SolicitudPrestamo(idCliente, monto, cuotas){
+							Console.WriteLine("Se ha aceptado su solicitud de prestamo.");
+						}
+						else {
+							Console.WriteLine("Se ha rechazado su solicitud de prestamo.");
+							Console.WriteLine("Las causas pueden ser las siguientes: ");
+							Console.WriteLine("\t- Tener entre 18 y 75 años de edad");
+							Console.WriteLine("\t- Tener al menos 6 meses de antiguedad laboral");
+							Console.WriteLine("\t- Tener un sueldo minimo de $5000");
+							Console.WriteLine("Además, según su tipo de cuenta: ");
+							Console.WriteLine("\t- Puede solicitar hasta $" + fachada.getMaximoMonto(idCliente));
+							Console.WriteLine("\t- Puede solicitar hasta " + fachada.getMaximasCuotas(idCliente) +" cuotas");
+						}
+						break;
+					}
+				default:
+					{
+						Main(args);
+						break;
+					}
+			}
         }
     }
 }
