e9097b3d8db31cf4a649288b91ebe5301b54f06a
Aye <Aye@AYE-PC>
2016-09-30 19:01:13 -0300

    fgdf

--- Ej4/EncriptadorCesar.cs
+++ Ej4/EncriptadorCesar.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Ej4
 {
-    class EncriptadorCesar : Encriptador
+     public class EncriptadorCesar : Encriptador
     {
+        public int iDesplazamiento;
+        public EncriptadorCesar (int pDesplazamiento)
+        {
+
+        }
+
+        public static string Encriptar(string pCadena)
+        {
+            string caracter = string.Empty;
+            for (int i = 0; i < pCadena.Length; i++)
+            {
+                string Letra = pCadena.Substring(i, 1);
+                int Codigo = Convert.ToInt16(Letra);
+                string codcaracter = (string)EncriptadorCesar;
+                caracter = caracter + codcaracter;
+            }
+            return caracter;
+        }
+
     }
 }
