bb4611e11b3d26bc7744be714c65c684b181068c
GiulianaMiret <dariela_77@hotmail.com>
2016-10-07 18:16:20 -0300

    Termine el TP4

---
+++ Ej4/Fachada.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej4
+{
+    class Fachada
+    {
+
+        public string EncriptarAES (string palabra)
+        {
+            EncriptadorAES encriptador = new EncriptadorAES("AES");
+            return (encriptador.Encriptar(palabra));
+        }
+
+        public string DesencriptarAES(string palabra)
+        {
+            EncriptadorAES encriptador = new EncriptadorAES("AES");
+            return (encriptador.Desencriptar(palabra));
+        }
+
+        public string EncriptarCesar(string palabra, int desplazamiento)
+        {
+            EncriptadorCesar encriptador = new EncriptadorCesar("Cesar", desplazamiento);
+            return (encriptador.Encriptar(palabra));
+        }
+
+        public string DesencriptarCesar(string palabra, int desplazamiento)
+        {
+            EncriptadorCesar encriptador = new EncriptadorCesar("Cesar", desplazamiento);
+            return (encriptador.Desencriptar(palabra));
+        }
+
+        public string EncriptarNulo(string palabra)
+        {
+            EncriptadorNulo encriptador = new EncriptadorNulo("Nulo");
+            return (encriptador.Encriptar(palabra));
+        }
+
+        public string DesencriptarNulo(string palabra)
+        {
+            EncriptadorNulo encriptador = new EncriptadorNulo("Nulo");
+            return (encriptador.Desencriptar(palabra));
+        }
+    }
+}
--- Ej4/Program.cs
+++ Ej4/Program.cs
@@ -10,12 +10,90 @@ namespace Ej4
     {
         static void Main(string[] args)
         {
-            EncriptadorCesar en = new EncriptadorCesar("Cesar", 2);
-            
-            Console.WriteLine(en.Encriptar("Hola"));
-            
-            Console.WriteLine(en.Desencriptar("Jqñc"));
-            Console.ReadKey();
+            Fachada fachada = new Fachada();
+            out1:
+            Console.Clear();
+            Console.WriteLine("Qué desea hacer?");
+            Console.WriteLine("1: Encriptar");
+            Console.WriteLine("2: Desencriptar");
+            Console.WriteLine("3: Salir");
+            string op = Console.ReadLine();
+            switch (op)
+            {
+                case "1":
+                    Console.WriteLine("Escriba la palabra que desea Encriptar:");
+                    string palabra = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Qué metodo utilizar para Encriptar?");
+                    Console.WriteLine("1: AES");
+                    Console.WriteLine("2: Cesar");
+                    Console.WriteLine("3: Nulo");
+                    Console.WriteLine("4: Salir");
+                    op = Console.ReadLine();
+                    Console.Clear();
+                    switch (op)
+                    {
+                        case "1":
+                            palabra = fachada.EncriptarAES(palabra);
+                            Console.WriteLine("Desea ver la palabra encriptada? (s/n)");
+                            if (Console.ReadLine() == "s")
+                            {
+                                Console.WriteLine(palabra);
+                            }
+                            goto out1;
+                        case "2":
+                            Console.WriteLine("Ingrese el desplazamiento:");
+                            int desplaz = Convert.ToInt32(Console.ReadLine());
+                            palabra = fachada.EncriptarCesar(palabra, desplaz);
+                            Console.WriteLine("Desea ver la palabra encriptada? (s/n)");
+                            if (Console.ReadLine() == "s")
+                            {
+                                Console.WriteLine(palabra);
+                            }
+                            goto out1;
+                        case "3":
+                            palabra = fachada.EncriptarNulo(palabra);
+                            Console.WriteLine("Desea ver la palabra encriptada? (s/n)");
+                            if (Console.ReadLine() == "s")
+                            {
+                                Console.WriteLine(palabra);
+                            }
+                            goto out1;
+                        default: goto out1;
+                    }
+                case "2":
+                    Console.WriteLine("Escriba la palabra que desea Desencriptar:");
+                    palabra = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Qué metodo utilizar para Desencriptar?");
+                    Console.WriteLine("1: AES");
+                    Console.WriteLine("2: Cesar");
+                    Console.WriteLine("3: Nulo");
+                    Console.WriteLine("4: Salir");
+                    op = Console.ReadLine();
+                    switch (op)
+                    {
+                        case "1":
+                            palabra = fachada.DesencriptarAES(palabra);
+                            Console.WriteLine("La palabra desencriptada es:");
+                            Console.WriteLine(palabra);
+                            goto out1;
+                        case "2":
+                            Console.WriteLine("Ingrese el desplazamiento:");
+                            int desplaz = Convert.ToInt32(Console.ReadLine());
+                            palabra = fachada.DesencriptarCesar(palabra, desplaz);
+                            Console.WriteLine("La palabra desencriptada es:");
+                            Console.WriteLine(palabra);
+                            goto out1;
+                        case "3":
+                            palabra = fachada.DesencriptarNulo(palabra);
+                            Console.WriteLine("La palabra desencriptada es:");
+                            Console.WriteLine(palabra);
+                            goto out1;
+                        default: goto out1;
+                    }
+            default: break;
+            }
         }
     }
 }
