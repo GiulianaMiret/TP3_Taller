5a8cd5477824b9293b423d2ad9bb2bc2b5082681
GiulianaMiret <dariela_77@hotmail.com>
2016-10-09 18:13:09 -0300

    Fin Ej4

--- Ej4/Program.cs
+++ Ej4/Program.cs
@@ -11,84 +11,81 @@ namespace Ej4
         static void Main(string[] args)
         {
             Fachada fachada = new Fachada();
-            out1:
+            string palabra = " ";
+            string opEnc = " ";
+            int desplaz = 0;
+            int aux = 0;
+            Console.Title = "Trabajo Practico N° 3 - Ejercicio 4";
+        out1:
             Console.Clear();
-            Console.WriteLine("Qué desea hacer?");
-            Console.WriteLine("1: Encriptar");
-            Console.WriteLine("2: Desencriptar");
-            Console.WriteLine("3: Salir");
+            Console.WriteLine("Qué desea hacer? \n\n\t1: Encriptar\n\t2: Desencriptar\n\n\t3: Salir");
             string op = Console.ReadLine();
+            Console.Clear();
             switch (op)
             {
                 case "1":
                     Console.WriteLine("Escriba la palabra que desea Encriptar:");
-                    string palabra = Console.ReadLine();
+                    palabra = Console.ReadLine();
                     Console.Clear();
-                    Console.WriteLine("Qué metodo utilizar para Encriptar?");
-                    Console.WriteLine("1: AES");
-                    Console.WriteLine("2: Cesar");
-                    Console.WriteLine("3: Nulo");
-                    Console.WriteLine("4: Salir");
-                    op = Console.ReadLine();
+                    Console.WriteLine("Qué metodo utilizar para Encriptar?\n\n\t1: AES\n\t2: Cesar\n\t3: Nulo\n\n\t4: Salir");
+                    opEnc = Console.ReadLine();
                     Console.Clear();
-                    switch (op)
+                    switch (opEnc)
                     {
                         case "1":
-                            palabra = fachada.EncriptarAES(palabra);
-                            Console.WriteLine("Desea ver la palabra encriptada? (s/n)");
-                            if (Console.ReadLine() == "s")
-                            {
-                                Console.WriteLine(palabra);
-                            }
+                            Console.WriteLine("Metodo:\n\tAES\nFrase:\n\t{0} \nFrase encriptada:\n\t{1}", palabra, palabra = fachada.EncriptarAES(palabra));
+                            Console.ReadKey();
                             goto out1;
                         case "2":
                             Console.WriteLine("Ingrese el desplazamiento:");
-                            int desplaz = Convert.ToInt32(Console.ReadLine());
-                            palabra = fachada.EncriptarCesar(palabra, desplaz);
-                            Console.WriteLine("Desea ver la palabra encriptada? (s/n)");
-                            if (Console.ReadLine() == "s")
-                            {
-                                Console.WriteLine(palabra);
-                            }
+                            desplaz = Convert.ToInt32(Console.ReadLine());
+                            aux = 1;
+                            Console.WriteLine("Metodo:\n\tCesar\nDesplazamiento:\t{0}\nFrase:\n\t{1}\nFrase encriptada:\n\t{2}", desplaz,palabra, palabra = fachada.EncriptarCesar(palabra, desplaz));
+                            Console.ReadKey();
                             goto out1;
                         case "3":
-                            palabra = fachada.EncriptarNulo(palabra);
-                            Console.WriteLine("Desea ver la palabra encriptada? (s/n)");
-                            if (Console.ReadLine() == "s")
-                            {
-                                Console.WriteLine(palabra);
-                            }
+                            Console.WriteLine("Metodo:\n\tNulo\nFrase:\n\t{0} \nFrase encriptada:\n\t{1}", palabra, palabra = fachada.EncriptarNulo(palabra));
+                            Console.ReadKey();
                             goto out1;
                         default: goto out1;
                     }
                 case "2":
+                    if (palabra != " ")
+                    {
+                        Console.WriteLine("Que desea desencriptar: \n\n\t1: La palabra anterior\n\t2: Una nueva palabra\n\n\t3: Salir");
+                        op = Console.ReadLine();
+                        if (op == "1")
+                        {
+                            op = opEnc;
+                            goto out2;
+                        }
+                        if (op == "3") { goto out1; }
+                    }
                     Console.WriteLine("Escriba la palabra que desea Desencriptar:");
                     palabra = Console.ReadLine();
                     Console.Clear();
-                    Console.WriteLine("Qué metodo utilizar para Desencriptar?");
-                    Console.WriteLine("1: AES");
-                    Console.WriteLine("2: Cesar");
-                    Console.WriteLine("3: Nulo");
-                    Console.WriteLine("4: Salir");
+                    Console.WriteLine("Qué metodo utilizar para Desencriptar?\n\n\t1: AES\n\t2: Cesar\n\t3: Nulo\n\n\t4: Salir");
                     op = Console.ReadLine();
+                out2:
+                    Console.Clear();
                     switch (op)
                     {
                         case "1":
-                            palabra = fachada.DesencriptarAES(palabra);
-                            Console.WriteLine("La palabra desencriptada es:");
-                            Console.WriteLine(palabra);
+                            Console.WriteLine("Metodo:\n\tAES \nFrase encriptada:\n\t{0}\nFrase desencriptadada: \n\t{1}", palabra, palabra = fachada.DesencriptarAES(palabra));
+                            Console.ReadKey();
                             goto out1;
                         case "2":
-                            Console.WriteLine("Ingrese el desplazamiento:");
-                            int desplaz = Convert.ToInt32(Console.ReadLine());
-                            palabra = fachada.DesencriptarCesar(palabra, desplaz);
-                            Console.WriteLine("La palabra desencriptada es:");
-                            Console.WriteLine(palabra);
+                            if (aux == 0)
+                            {
+                                Console.WriteLine("Ingrese el desplazamiento:");
+                                desplaz = Convert.ToInt32(Console.ReadLine());
+                            }
+                            Console.WriteLine("Metodo:\n\tCesar\nDesplazamiento:\t{2}\nFrase encriptada:\n\t{0}\nFrase desencriptada:\n\t{1}", palabra, palabra = fachada.DesencriptarCesar(palabra, desplaz), desplaz);
+                            Console.ReadKey();
                             goto out1;
                         case "3":
-                            palabra = fachada.DesencriptarNulo(palabra);
-                            Console.WriteLine("La palabra desencriptada es:");
-                            Console.WriteLine(palabra);
+                            Console.WriteLine("Metodo:\n\tNulo\nFrase encriptada:\n\t{0}\nFrase desencriptada:\n\t{1}", palabra, palabra = fachada.DesencriptarNulo(palabra));
+                            Console.ReadKey();
                             goto out1;
                         default: goto out1;
                     }
