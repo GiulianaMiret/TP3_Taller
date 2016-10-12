3baa5de8b57bcc6ca5556ff12759cc18672069d8
Facundo <farebord@gmail.com>
2016-10-12 19:05:18 -0300

    Agrego encriptador RSA

---
+++ Ej5/EncriptadorRSA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Ej5
+{
+
+	class EncriptadorRSA : Encriptador
+	{
+		private string key = "34n5jk2b354jkhjk345";
+		private RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+		public RSAParameters parameters = new RSAParameters();
+
+		public EncriptadorRSA() : base("RSA") {
+			provider.ImportParameters(parameters);
+		}
+
+		public override string Encriptar(string pCadena)
+		{
+			var cadenaEncriptada = provider.Encrypt(System.Text.Encoding.UTF8.GetBytes(pCadena), true);
+			return System.Text.Encoding.UTF8.GetString(cadenaEncriptada);
+		}
+
+		public override string Desencriptar(string pCadena)
+		{
+			return System.Text.Encoding.UTF8.GetString(provider.Decrypt(System.Text.Encoding.UTF8.GetBytes(pCadena), true));
+		}
+
+    }
+}
